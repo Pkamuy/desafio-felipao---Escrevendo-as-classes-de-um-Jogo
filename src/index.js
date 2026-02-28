bb3f65b8aca4c6b379bb14e70245a904b0c7e73d
Pkamuy <pedroanderson.cardoso@gmail.com>
2026-02-28 15:56:18 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 let criaHeroi = {
-    nome: "Pedro"
-    idade: "39"
-    tipo: ["guerreiro", "mago", "monge", "ninja"]
-}
+    nome: "Pedro",
+    idade: 39,
+    tipo: {
+        0: ["guerreiro","espada"],
+        1: ["mago","magia"],
+        2: ["monge","artes marciais"], 
+        3: ["ninja","shuriken"],
+        }
+    }
+criacaoPersonagem(criaHeroi)
 
-console.log(criaHeroi)
+function criacaoPersonagem (criaHeroi) {
+        for(let index in criaHeroi.tipo) {
+        let [tipoClasse,armaUsada] = criaHeroi.tipo[index]
+        console.log(`"O" ${tipoClasse} "atacou usando" ${armaUsada}`)
+    }
+}
